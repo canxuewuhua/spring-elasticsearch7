9ab142c0c08a88c34d6bce05be196ffcb817b92b
981162250@qq.com <yongqiang.zhu@easydcmm.com>
2023-05-13 10:45:24 +0800

    first commit

---
+++ src/main/java/com/huolieniao/SpringElasticSearch7Main.java
@@ -0,0 +1,12 @@
+package com.huolieniao;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringElasticSearch7Main {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringElasticSearch7Main.class, args);
+    }
+}
